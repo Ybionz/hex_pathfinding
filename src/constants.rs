14bc9f2823a6a9d48521dd3cea6448942eee41b4
Ybionz <ybionz@gmail.com>
2023-09-19 21:12:36 +0200

    Rename file to constants to avoid confusion with core modules.

---
+++ src/constants.rs
@@ -0,0 +1,11 @@
+use lazy_static::lazy_static;
+
+pub const SIZE: f64 = 20.;
+pub const H: f64 = SIZE;
+pub const START_X: f64 = 20.;
+pub const START_Y: f64 = 20.;
+pub const WALL_THICKNESS: f64 = 3.;
+
+lazy_static! {
+    pub static ref W: f64 = (3.0_f64).sqrt() / 2. * SIZE;
+}
